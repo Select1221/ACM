24bfc928f22a1e6a550c12088d1220bf610c53da
Select1221 <szc30204@163.com>
2022-09-28 15:17:00 +0800

    "通过"

--- p3循环结构/R百钱百鸡.cpp
+++ p3循环结构/R百钱百鸡.cpp
@@ -9,6 +9,7 @@ int main(int argc, char const *argv[])
         if ((n - 7 * i) % 4 == 0)
         {
             x++;
+            // printf("公鸡=%d 母鸡=%d 小鸡=%d\n", i, (n - 7 * i) / 4, n - i - (n - 7 * i) / 4);
         }
     }
     printf("%d\n", x);
@@ -18,3 +19,4 @@ int main(int argc, char const *argv[])
 // a*15+b*9+n-a-b=n*3
 // a*14+b*8=n*2
 // a*7+b*4=n
+// b*4=n-a*7

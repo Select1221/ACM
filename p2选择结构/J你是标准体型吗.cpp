a209fda34a2e4f167e31c7f92232b65848d2b51e
Select1221 <szc30204@163.com>
2022-10-02 19:27:38 +0800

    通过

--- p2选择结构/J你是标准体型吗.cpp
+++ p2选择结构/J你是标准体型吗.cpp
@@ -1,22 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-    double weight,height,BMI;
-    scanf("%lf %lf",&weight,&height);
-    BMI=weight/(height*height);
-    if (BMI<18.4)
+    double weight, height, BMI;
+    scanf("%lf %lf", &weight, &height);
+    BMI = weight / (height * height);
+    if (BMI < 18.4)
     {
         printf("Underweight ");
-    }else if (BMI>=18.4 && BMI<24.0)
+    }
+    else if (BMI >= 18.4 && BMI < 24.0)
     {
         printf("Normal");
-    }else if (BMI>=24.0 && BMI<28.0)
+    }
+    else if (BMI >= 24.0 && BMI < 28.0)
     {
         printf("Overweight");
-    }else{
+    }
+    else
+    {
         printf("Obesity");
     }
-    
+
     return 0;
 }
--- p3循环结构/J房子.cpp
+++ p3循环结构/J房子.cpp
@@ -2,20 +2,24 @@
 
 int main(int argc, char const *argv[])
 {
-    int N, K, M = 1, home = 200;
+    int N, K, M = 1;
+    double home = 200;
     scanf("%d %d", &N, &K);
     while (M <= 20)
     {
         if (N * M >= home)
         {
             printf("%d\n", M);
-            return 0;
+            break;
         }
         home *= 1 + K / 100.0;
         M++;
     }
-
-    printf("Impossible\n");
+    
+    if (M > 20)
+    {
+        printf("Impossible\n");
+    }
 
     return 0;
 }
